bbe4428ed49ea484fc631d4775195d3e12713ca3
emily883 <emily.moraima.cruz@hotmail.com>
2021-07-05 18:30:46 -0400

    otro pull de prueba

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ alert("hola niños")
 
 const obj = {
     saludo: "que onda capos",
-    nombre: ["franco", "Eze", "no hice pull request para poner esto"]
+    nombre: ["franco", "Eze", "no hice pull request para poner esto"],
+    despedida: "Adios nos vemos!!"
 }
