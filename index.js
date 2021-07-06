5d2d3c7deb04f8079451a90e39f834afc67da2c3
ezequielaguilera1993 <75750236+ezequielaguilera1993@users.noreply.github.com>
2021-07-05 22:49:13 -0300

    modifique el dev

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 alert("hola niños")
 
-const obj = {
-    saludo: "que onda capos",
-    nombre: ["franco", "Eze", "no hice pull request para poner esto"],
-    despedida: "Adios nos vemos!!"
-}
+console.log("YO SOY DEV!!")
